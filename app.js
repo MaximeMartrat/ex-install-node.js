e258e7009a1009cc97e0551b66968907f6bb6e79
MaximeMartrat <maximemartrat@outlook.fr>
2023-02-06 15:37:24 +0100

    maj ex

---
+++ app.js
@@ -0,0 +1,27 @@
+//création des constantes pour exporter les modules express et fs  
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+//Demande au serveur d'ecouter sur le port 4000
+app.listen(4000, () => {
+    //Affichage d'un message pour confirmer que l'app tourne bien sur le port 4000
+    console.log('L\'app tourne sur le port 4000');
+    });
+
+//définition de la target pour les données
+app.get("/data", (request, response) => {
+    //lecture du fichier menu.json en utilisant fs.readFile()
+    fs.readFile("menu.json", (err, data) => {
+        if (err) {
+            //si une erreur se produit lors de la lecture du fichier on envoi une réponse JSON avec le code d'erreur 500
+            response.status(500).json({
+                message: "Une erreur lors de la lecture des données",
+                error: err
+            });
+        } else {
+            //sinon on envoi la reponse en JSON
+            response.status(200).json(JSON.parse(data));
+        }
+    });
+});
